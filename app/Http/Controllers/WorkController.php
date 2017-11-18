12b7cfc6477c2be7ef3c2c8a9f950a596e5780ae
AlenHuang MBP <alenhung@gmail.com>
2017-11-18 09:21:18 +0800

    尚未完成

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Works;
+use DB;
+use Session;
+use Input;
+use Image;
 
 class WorkController extends Controller
 {
@@ -35,6 +41,44 @@ class WorkController extends Controller
     public function store(Request $request)
     {
         //
+        $this->validate($request, [
+          'title' => 'required|max:255',
+          'slogan' => 'sometimes|max:255',
+          'description' => 'sometimes|max:255',
+          'location' => 'required|max:255',
+          'service_location' => 'sometimes|max:255',
+          'land_plan' => 'sometimes|max:255',
+          'land_size' => 'sometimes|max:255',
+          'households' => 'sometimes|max:255',
+          'unit_area' => 'sometimes|max:255',
+          'public_ratio' => 'sometimes|max:255',
+          'tall' => 'sometimes|max:255',
+          'completion_date' => 'sometimes|max:255',
+          'public_ratio' => 'sometimes|max:255',
+          'confirmed' => 'required',
+
+        ]);
+        if($request->hasFile('project_image')){
+            $project_image = $request->file('project_image');
+            $filename = time() . '.' . $project_image->getClientOriginalExtension();
+            Image::make($project_image)->resize(300, 300)->save( public_path('/uploads/avatars/' . $filename ) );
+
+            $work = new Work();
+            $work->title = $request->title;
+            $work->slogan = $request->slogan;
+            $work->description = $request->description;
+            $work->service_location = $request->service_location;
+            $work->location = $request->location;
+            $work->land_plan = $request->land_plan;
+            $work->land_size = $request->land_size;
+            $work->households = $request->households;
+            $work->unit_area = $request->unit_area;
+            $work->public_ratio = $request->public_ratio;
+            $work->tall = $request->tall;
+            $work->completion_date = $request->completion_date;
+            $work->project_image = $filename;
+            $work->save();
+        }
     }
 
     /**
