0463f8e63bb8c4d59e7a4d58858f293307dc6308
AlenHuang MBP <alenhung@gmail.com>
2017-11-27 01:39:32 +0800

    add jquery

--- resources/views/manage/working_photos/create.blade.php
+++ resources/views/manage/working_photos/create.blade.php
@@ -49,7 +49,7 @@
     </form>
   </div> <!-- end of .flex-container -->
 @endsection
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="{{ asset('js/dropzone.js') }}"></script>
 
 @section('scripts')
