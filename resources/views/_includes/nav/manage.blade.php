8144301b282dddfc792cabbf920c713f831ab3a9
alenhung <alenhung@gmail.com>
2017-11-26 16:52:52 +0800

    增加font awesome icon

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -7,16 +7,17 @@
       熱銷建案－管理端
     </p>
     <ul class="menu-list">
-      <li><a href="{{route('works.index')}}">熱銷建案列表</a></li>
-      <li><a href="{{route('works.create')}}">新增熱銷建案</a></li>
+      <li><a href="{{route('works.index')}}"><i class="fa fa-list-ul m-r-10" aria-hidden="true"></i>熱銷建案列表</a></li>
+      <li><a href="{{route('works.create')}}"><i class="fa fa-plus-square m-r-10" aria-hidden="true"></i>新增熱銷建案</a></li>
     </ul>
     <hr>
     <p class="menu-label">
       在建工程－管理端
     </p>
     <ul class="menu-list">
-      <li><a href="{{route('workings.index')}}">在建工程列表</a></li>
-      <li><a href="{{route('workings.create')}}">新增在建工程</a></li>
+      <li><a href="{{route('workings.index')}}"><i class="fa fa-list-ul m-r-10" aria-hidden="true"></i>在建工程列表</a></li>
+      <li><a href="{{route('workings.create')}}"><i class="fa fa-plus-square m-r-10" aria-hidden="true"></i>新增在建工程</a></li>
+      <li><a href="{{route('workings.create')}}"><i class="fa fa-file-image-o m-r-10" aria-hidden="true"></i>新增在建工程相片</a></li>
     </ul>
     <hr>
   </aside>
